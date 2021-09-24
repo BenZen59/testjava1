cb729919ebccb7447f5ff5bd886460f04dd28651
BenZen59 <benoit.zenone@gmail.com>
2021-09-24 17:17:33 +0200

    Added new files

---
+++ script.js
@@ -0,0 +1,36 @@
+function addCharacter (name,pv,pm,force,vitesse,magie,vitalité,chance)
+{
+    const body = document.querySelector('body');
+    const character = document.createElement('div');
+
+    const newName = document.createElement('p');
+    const newPv = document.createElement('p');
+    const newPm = document.createElement('p');
+    const newForce = document.createElement('p');
+    const newVitesse = document.createElement('p');
+    const newMagie = document.createElement('p');
+    const newVitalité = document.createElement('p');
+    const newChance = document.createElement('p');
+
+    newName.innerText ="Nom : " + name;
+    newPv.innerText = "PV : " + pv;
+    newPm.innerText = "PM : " + pm;
+    newForce.innerText = "Force :" + force;
+    newVitesse.innerText = "Vitesse : " + vitesse;
+    newMagie.innerText = "Magie : " + magie;
+    newVitalité.innerText = "Vitalité : " + vitalité;
+    newChance.innerText = "Chance : " + chance;
+
+    character.appendChild(newName);
+    character.appendChild(newPv);
+    character.appendChild(newPm);
+    character.appendChild(newForce);
+    character.appendChild(newVitesse);
+    character.appendChild(newMagie);
+    character.appendChild(newVitalité);
+    character.appendChild(newChance);
+
+    body.appendChild(character);
+}
+
+addCharacter('Benku',300,200,10,8,5,9,5,5);
